d957d7dbc32e9802a68b596e4ab5719d002eaa43
diegoesp89 <diego.espinoza.vasquez@gmail.com>
2021-04-03 22:06:08 -0300

    asd

--- zombite.c
+++ zombite.c
@@ -22,37 +22,32 @@
 #include "vrambuf.h"
 //#link "vrambuf.c"
 
-/*{pal:"nes",layout:"nes"}*/
-const char PALETTE[32] = { 
-  0x03,			// screen color
-
-  0x11,0x30,0x27,0x0,	// background palette 0
-  0x1c,0x20,0x2c,0x0,	// background palette 1
-  0x00,0x10,0x20,0x0,	// background palette 2
-  0x06,0x16,0x26,0x0,   // background palette 3
-
-  0x16,0x35,0x24,0x0,	// sprite palette 0
-  0x00,0x37,0x25,0x0,	// sprite palette 1
-  0x0d,0x2d,0x3a,0x0,	// sprite palette 2
-  0x0d,0x27,0x2a	// sprite palette 3
-};
 
 // setup PPU and tables
 void setup_graphics() {
   // clear sprites
-  oam_clear();
+  //oam_clear();
   // set palette colors
-  pal_all(PALETTE);
+  pal_col(0,0x03);
+  pal_col(1,0x14);
+  pal_col(2,0x10);
+  pal_col(3,0x30);
 }
 
 void main(void)
 {
+  int x;
   setup_graphics();
   // draw message  
   vram_adr(NTADR_A(2,2));
-  vram_write("HELLO, WORLD!", 12);
+  vram_write("Strife es la ostia!", 19);
   // enable rendering
   ppu_on_all();
+  
+  for(x=0;x<500;x++){
+    ppu_wait_frame();
+  }
+  ppu_off();
   // infinite loop
   while(1) {
   }
